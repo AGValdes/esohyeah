e43cf8ca03d6080f2c588126cfabfb567df730ed
Ameilia Valdes <valdes.ameilia@gmail.com>
2020-09-16 15:27:14 -0700

    Adding initial js files and added the name prompt

---
+++ username.js
@@ -0,0 +1,6 @@
+var userName = prompt('Please Enter Your Name');
+    if (userName == 'Ameilia') {
+        document.write('Whattup' + userName)
+    } else {document.write ('Welcome ' + userName + '!');
+}
+
