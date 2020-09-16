129170329723f8b9674b190506f722002431efae
Ameilia Valdes <valdes.ameilia@gmail.com>
2020-09-16 16:10:41 -0700

    added first question

---
+++ question1.js
@@ -0,0 +1,13 @@
+var question1 = prompt('What do you want to be your main combat attribute: magic, stamina, or stealth?');
+    if (question1 == 'magic') {
+        document.write('You should be a High Elf!')
+    }
+    if (question1 == 'stamina') {
+        document.write('You should be a Wood Elf!')
+    }
+    if (question1 == "stealth") {
+        document.write("You should be a Khajiit!")
+    } 
+    
+
+
