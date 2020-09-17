f3429ac6b67dc57ea5510c0a68a258fbc28ae0c5
Ameilia Valdes <valdes.ameilia@gmail.com>
2020-09-17 12:34:50 -0700

    removed old js files

--- js/username.js
+++
@@ -1,6 +0,0 @@
-var userName = prompt('Please Enter Your Name');
-    if (userName == 'Ameilia') {
-        document.write('Whattup' + userName)
-    } else {document.write ('Welcome ' + userName + '!');
-}
-
