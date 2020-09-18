3a9dd24760495bb98c835c7cba015833352e1648
Ameilia Valdes <valdes.ameilia@gmail.com>
2020-09-18 11:12:35 -0700

    fixed the correct answer bug

--- script.js
+++ script.js
@@ -10,11 +10,12 @@ function areRpgsTheBest() {
 var correctNumber = 7;
 for (var i=0; i < 4; i++){
     var userGuess = prompt ('Guess a number between 1 and 10');
-     if ((i === 3) && (correctNumber !== userGuess)) {
+     if ((i === 3) && (correctNumber !== parseInt(userGuess))) {
         alert('close, but no cigar!');
    }
-    else if (userGuess === correctNumber) {
+    else if (parseInt(userGuess) === correctNumber) {
         alert('You are Correct!');
+        break;
    }
     else if (userGuess !== correctNumber) {
        alert('Nope, Try Again!');
@@ -23,7 +24,7 @@ for (var i=0; i < 4; i++){
 
 function askUserNameQuestion(){
     var userName = prompt('Please Enter Your Name');
-        if (userName == 'Ameilia') {
+        if (userName === 'Ameilia') {
             document.write('Whattup' + userName)
         } else {document.write ('Welcome ' + userName + '!');
     }
